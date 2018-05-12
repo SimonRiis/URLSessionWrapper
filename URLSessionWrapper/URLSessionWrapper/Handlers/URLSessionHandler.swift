8230695135290099c73be10b486aa8933ee400f1
Simon Riis Olesen <simonriisolesen@Simons-MacBook-Pro.local>
2018-05-12 16:27:23 +0200

    First commit

---
+++ URLSessionWrapper/URLSessionWrapper/Handlers/URLSessionHandler.swift
@@ -0,0 +1,50 @@
+//
+//  URLSessionHandler.swift
+//  URLSessionWrapper
+//
+//  Created by Simon Riis Olesen on 10/02/2018.
+//  Copyright © 2018 URLSessionWrapper. All rights reserved.
+//
+
+import Foundation
+public class URLSessionHandler:Callable{
+    
+    public init() {}
+    public func call(request: URLRequest, result: @escaping ([String : Any]?) -> Void) {
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            
+            if let httpResponse = response as? HTTPURLResponse {
+                
+                let statusCode = httpResponse.statusCode
+                var json:[String: Any] = [
+                    
+                    "statusCode": statusCode
+                    
+                ]
+                if let data = data, let responseJSON = try? JSONSerialization.jsonObject(with: data, options: []){
+                    
+                    if let response = responseJSON as? [String: Any]{
+                        
+                        json["responseData"] = response
+                        
+                    }
+                    
+                }else{
+                    if let error = error{
+                        json["error"] = error.localizedDescription
+                    }
+                }
+                result(json)
+                
+            }else{
+                result(nil)
+            }
+            
+            
+        }
+        
+        task.resume()
+        
+        
+    }
+}
